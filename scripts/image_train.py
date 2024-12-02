34de18ef7be911a95651281eafed6c246e036041
sujith-1610 <sujithgundapu@gmail.com>
2024-12-02 20:29:04 +0530

    Update image_train.py

--- scripts/image_train.py
+++ scripts/image_train.py
@@ -93,7 +93,7 @@ def create_argparser():
         resume_checkpoint="",
         use_fp16=False,
         fp16_scale_growth=1e-3,
-        save_model_path="/kaggle/working/output",  # Path to save the trained model
+        save_model_path="/kaggle/working",  # Path to save the trained model
     )
     defaults.update(model_and_diffusion_defaults())
     parser = argparse.ArgumentParser()

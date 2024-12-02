7e5b3956b3c4d93f7e45e1033c9ba3cd2c0d887c
sujith-1610 <sujithgundapu@gmail.com>
2024-12-02 19:05:17 +0530

    Update image_train.py

--- scripts/image_train.py
+++ scripts/image_train.py
@@ -83,7 +83,7 @@ def create_argparser():
         schedule_sampler="uniform",
         lr=1e-4,
         weight_decay=0.0,
-        lr_anneal_steps=10000,
+        lr_anneal_steps=1000,
         batch_size=1,
         microbatch=-1,  # -1 disables microbatches
         ema_rate="0.9999",  # comma-separated list of EMA values
